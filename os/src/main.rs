6ad85edd5131357f223236505b6136bd8001d4c6
Qing Wang <wangqing7171@gmail.com>
2026-02-12 20:18:43 +0800

    ch4/1-2: mm: Heap code.
    Signed-off-by: Qing Wang <wangqing7171@gmail.com>

--- os/src/main.rs
+++ os/src/main.rs
@@ -2,6 +2,7 @@
 #![no_main]
 // #![deny(missing_docs)]
 #![deny(warnings)]
+#![feature(alloc_error_handler)]
 
 #[macro_use]
 mod console;
@@ -14,17 +15,21 @@ mod loader;
 mod task;
 pub mod config;
 mod timer;
+pub mod mm;
 
 use core::arch::global_asm;
+use mm::heap_allocator::init_heap;
+extern crate alloc;
+
 global_asm!(include_str!("entry.asm"));
 global_asm!(include_str!("link_app.S"));
 
-fn clear_bss() {
-    unsafe extern "C" {
-        safe fn sbss();
-        safe fn ebss();
-    }
+unsafe extern "C" {
+    safe fn sbss();
+    safe fn ebss();
+}
 
+fn clear_bss() {
     (sbss as usize..ebss as usize).for_each(|a| unsafe {
         (a as *mut u8).write_volatile(0);
     });
@@ -32,6 +37,8 @@ fn clear_bss() {
 
 #[unsafe(no_mangle)]
 pub fn rust_main() -> ! {
+    init_heap();
+    heap_test();
     clear_bss();
     println!("\x1b[31m hello rCore! \x1b[0m");
     trap::init();
@@ -43,4 +50,23 @@ pub fn rust_main() -> ! {
     panic!("Unreachable in rust_main!");
 }
 
-
+#[allow(unused)]
+pub fn heap_test() {
+    use alloc::boxed::Box;
+    use alloc::vec::Vec;
+    let bss_range = sbss as usize..ebss as usize;
+    let a = Box::new(5);
+    assert_eq!(*a, 5);
+    assert!(bss_range.contains(&(a.as_ref() as *const _ as usize)));
+    drop(a);
+    let mut v: Vec<usize> = Vec::new();
+    for i in 0..500 {
+        v.push(i);
+    }
+    for i in 0..500 {
+        assert_eq!(v[i], i);
+    }
+    assert!(bss_range.contains(&(v.as_ptr() as usize)));
+    drop(v);
+    println!("heap_test passed!");
+}
