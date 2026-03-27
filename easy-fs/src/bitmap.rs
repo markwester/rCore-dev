a23f41236a355e133542776e582530bf51169f4b
Qing Wang <wangqing7171@gmail.com>
2026-03-27 18:13:39 +0800

    ch6-2: Impl easy-fs
    Signed-off-by: Qing Wang <wangqing7171@gmail.com>

--- easy-fs/src/bitmap.rs
+++ easy-fs/src/bitmap.rs
@@ -1,15 +1,18 @@
 //! bitmap for block allocation
 
-use crate::block_cache::get_block_cache;
-use alloc::sync::Arc;
-use crate::block_dev::BlockDevice;
 use crate::BLOCK_SZ;
+use crate::block_cache::get_block_cache;
+use crate::block_dev::BlockDevice;
+use alloc::sync::Arc;
 
 pub struct Bitmap {
     start_block_id: usize,
     blocks: usize,
 }
 
+// bitmap save to blocks
+// blocks -> number of blocks
+// [start_block_id, start_block_id + blocks)
 impl Bitmap {
     pub fn new(start_block_id: usize, blocks: usize) -> Self {
         Self {
@@ -26,30 +29,42 @@ const BLOCK_BITS: usize = BLOCK_SZ * 8;
 impl Bitmap {
     pub fn alloc(&self, block_device: &Arc<dyn BlockDevice>) -> Option<usize> {
         for block_id in 0..self.blocks {
-            let pos = get_block_cache(
-                block_id + self.start_block_id as usize,
-                Arc::clone(block_device),
-            )
-            .lock()
-            .modify(0, |bitmap_block: &mut BitmapBlock| {
-                if let Some((bits64_pos, inner_pos)) = bitmap_block
-                    .iter()
-                    .enumerate()
-                    .find(|(_, bits64)| **bits64 != u64::MAX)
-                    .map(|(bits64_pos, bits64)| {
-                        (bits64_pos, bits64.trailing_ones() as usize)
-                    }) {
-                    // modify cache
-                    bitmap_block[bits64_pos] |= 1u64 << inner_pos;
-                    Some(block_id * BLOCK_BITS + bits64_pos * 64 + inner_pos as usize)
-                } else {
-                    None
-                }
-            });
+            let pos = get_block_cache(self.start_block_id + block_id, Arc::clone(block_device))
+                .lock()
+                .modify(0, |bitmap_block: &mut BitmapBlock| {
+                    if let Some((bits64_pos, inner_pos)) = bitmap_block
+                        .iter()
+                        .enumerate()
+                        .find(|(_, bits64)| **bits64 != u64::MAX)
+                        .map(|(bits64_pos, bits64)| (bits64_pos, bits64.trailing_ones() as usize))
+                    {
+                        bitmap_block[bits64_pos] |= 1u64 << inner_pos;
+                        Some(block_id * BLOCK_SZ + bits64_pos * 64 + inner_pos as usize)
+                    } else {
+                        None
+                    }
+                });
             if pos.is_some() {
                 return pos;
             }
         }
         None
     }
+
+    pub fn dealloc(&self, block_device: &Arc<dyn BlockDevice>, pos: usize) {
+        let (block_id, bits64_pos, inner_pos) = decode_pos(pos);
+        get_block_cache(block_id + self.start_block_id, Arc::clone(block_device))
+            .lock()
+            .modify(0, |bitmap_block: &mut BitmapBlock| {
+                assert!(bitmap_block[bits64_pos] & (1u64 << inner_pos) != 0);
+                bitmap_block[bits64_pos] &= !(1u64 << inner_pos);
+            });
+    }
+}
+
+fn decode_pos(pos: usize) -> (usize, usize, usize) {
+    let block_id = pos / BLOCK_BITS;
+    let bits64_pos = pos % BLOCK_BITS / 64;
+    let inner_pos = pos % BLOCK_BITS % 64;
+    (block_id, bits64_pos, inner_pos)
 }
--- easy-fs/src/layout.rs
+++ easy-fs/src/layout.rs
@@ -1,5 +1,11 @@
 //! fs layout
 
+use crate::BLOCK_SZ;
+use crate::block_cache::get_block_cache;
+use crate::block_dev::BlockDevice;
+use alloc::sync::Arc;
+use alloc::vec::Vec;
+
 /// Magic number for sanity check
 const EFS_MAGIC: u32 = 0x3b800001;
 
@@ -32,5 +38,112 @@ impl SuperBlock {
     }
     pub fn is_valid(&self) -> bool {
         self.magic == EFS_MAGIC
+    }
+}
+
+const INODE_DIRECT_COUNT: usize = 28;
+const INODE_INDIRECT1_COUNT: usize = BLOCK_SZ / 4;
+const INODE_INDIRECT2_COUNT: usize = INODE_INDIRECT1_COUNT * BLOCK_SZ / 4;
+const INODE_DIRECT_BOUND: usize = INODE_DIRECT_COUNT;
+const INODE_INDIRECT1_BOUND: usize = INODE_DIRECT_BOUND + INODE_INDIRECT1_COUNT;
+const INODE_INDIRECT2_BOUND: usize = INODE_INDIRECT1_BOUND + INODE_INDIRECT2_COUNT;
+// direct can be save INODE_DIRECT_COUNT blocks
+// indirect1 -> BLOCK_SZ / 4 blocks
+// indirect2 -> BLOCK_SZ / 4 * BLOCK_SZ / 4 blocks
+// save disk block id
+#[repr(C)]
+pub struct DiskInode {
+    pub size: u32,
+    pub direct: [u32; INODE_DIRECT_COUNT],
+    pub indirect1: u32,
+    pub indirect2: u32,
+    type_: DiskInodeType,
+}
+
+#[derive(PartialEq)]
+pub enum DiskInodeType {
+    File,
+    Directory,
+}
+
+type IndirectBlock = [u32; BLOCK_SZ / 4];
+
+impl DiskInode {
+    /// indirect1 and indirect2 block are allocated only when they are needed.
+    pub fn init(&mut self, type_: DiskInodeType) {
+        self.size = 0;
+        self.direct.iter_mut().for_each(|v| *v = 0);
+        self.indirect1 = 0;
+        self.indirect2 = 0;
+        self.type_ = type_;
+    }
+    pub fn is_dir(&self) -> bool {
+        self.type_ == DiskInodeType::Directory
+    }
+    pub fn is_file(&self) -> bool {
+        self.type_ == DiskInodeType::File
+    }
+
+    // get block id in disk
+    pub fn get_block_id(&self, inner_id: u32, block_device: &Arc<dyn BlockDevice>) -> u32 {
+        let inner_id = inner_id as usize;
+        if inner_id < INODE_DIRECT_COUNT {
+            self.direct[inner_id]
+        } else if inner_id < INODE_INDIRECT1_BOUND {
+            let indirect1_id = inner_id - INODE_DIRECT_BOUND;
+            get_block_cache(self.indirect1 as usize, Arc::clone(block_device))
+                .lock()
+                .read(0, |indirect_blk: &IndirectBlock| indirect_blk[indirect1_id])
+        } else {
+            assert!(inner_id < INODE_INDIRECT2_BOUND);
+            let indirect2_id = inner_id - INODE_INDIRECT2_BOUND;
+            let inner_id1 = indirect2_id / INODE_INDIRECT1_COUNT;
+            let inner_id2 = indirect2_id % INODE_INDIRECT1_COUNT;
+            // get indirect1 block id
+            let indirect1_blk = get_block_cache(self.indirect2 as usize, Arc::clone(block_device))
+                .lock()
+                .read(0, |indirect2_blk: &IndirectBlock| indirect2_blk[inner_id1]);
+            get_block_cache(indirect1_blk as usize, Arc::clone(block_device))
+                .lock()
+                .read(0, |indirect1_blk: &IndirectBlock| indirect1_blk[inner_id2])
+        }
+    }
+
+    /// cal data blocks needed for this size.
+    pub fn data_blocks(&self) -> u32 {
+        Self::_data_blocks(self.size)
+    }
+    fn _data_blocks(size: u32) -> u32 {
+        // ceil(size / BLOCK_SZ)
+        (size + BLOCK_SZ as u32 - 1) / BLOCK_SZ as u32
+    }
+    /// Return number of blocks needed include indirect1/2.
+    pub fn total_blocks(size: u32) -> u32 {
+        let data_blocks = Self::_data_blocks(size) as usize;
+        let mut total = data_blocks as usize;
+        // indirect1
+        if data_blocks > INODE_DIRECT_COUNT {
+            total += 1;
+        }
+        // indirect2
+        if data_blocks > INODE_INDIRECT1_BOUND {
+            total += 1;
+            // sub indirect1
+            total += (data_blocks - INODE_INDIRECT1_BOUND + INODE_INDIRECT1_COUNT - 1) / INODE_INDIRECT1_COUNT;
+        }
+        total as u32
+    }
+    pub fn blocks_num_needed(&self, new_size: u32) -> u32 {
+        assert!(new_size >= self.size);
+        Self::total_blocks(new_size) - Self::total_blocks(self.size)
+    }
+
+    pub fn increase_size(
+        &mut self,
+        new_size: u32,
+        new_blocks: Vec<u32>,
+        block_device: &Arc<dyn BlockDevice>,
+    ) {
+
     }
 }
